c3eeca21033430540fa4213d1073533f639a84ab
unknown <todiafn@gmail.com>
2025-11-18 14:46:06 -0300

    Tirei os comentarios da client_view

--- view/client_view.py
+++ view/client_view.py
@@ -4,7 +4,6 @@ from services.sessao_service import listar_sessoes
 from services.ingresso_service import vender_ingresso, listar_meus_ingressos
 
 def menu_cliente(usuario_logado):
-    # usuario_logado é uma tupla (id, nome, email, fone, senha) vinda do banco
     id_cliente = usuario_logado[0]
     nome_cliente = usuario_logado[1]
 
@@ -38,13 +37,11 @@ def menu_cliente(usuario_logado):
                 print("\n--- Compra de Ingresso ---")
                 id_sessao = get_int_input("Digite o ID da Sessão desejada: ")
                 assento = input("Escolha seu assento (ex: A1): ")
-                # Aqui definimos o preço fixo ou calculamos, vou por fixo pra simplificar
                 valor = 25.00 
                 print(f"Valor do ingresso: R$ {valor}")
                 confirmar = input("Confirmar compra? (s/n): ")
                 
                 if confirmar.lower() == 's':
-                    # Note que passamos o id_cliente logado automaticamente!
                     vender_ingresso(id_cliente, id_sessao, assento, valor)
                 else:
                     print("Compra cancelada.")
@@ -54,7 +51,6 @@ def menu_cliente(usuario_logado):
                 meus_tickets = listar_meus_ingressos(id_cliente)
                 print("\n--- Seus Ingressos ---")
                 for t in meus_tickets:
-                    # Agora mostra a sala (t[3])
                     print(f"Filme: {t[1]} | Data: {t[2]} | Sala: {t[3]} | Assento: {t[4]}")
                 pause()
 
